fcee357f92772b729eaf4a4d910006cebae4cdb4
Ketan Mathur <mathurketan3@gmail.com>
2025-07-13 13:31:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from datasets import load_dataset
+
 from transformers import pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
